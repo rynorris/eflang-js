c490d0c8e3f725edd9eb21cce7c51da7149430ed
Ryan Norris <rynorris@gmail.com>
2022-10-16 15:36:13 +0800

    More tests

--- eflang/eflang/interpreter/interpreter.spec.ts
+++ eflang/eflang/interpreter/interpreter.spec.ts
@@ -1,33 +1,130 @@
 import { Interpreter } from './interpreter';
 import { SparseTape } from './sparse-tape';
-import sinon from "sinon";
 import { ArraySource } from './array-source';
-import { IO, LoopEnd, LoopStart, Rest } from './api';
+import { Instruction, IO, LoopEnd, LoopStart, Note, Rest } from './api';
 
-it('should return the correct value', async () => {
-  const sendOutput = sinon.spy();
-  const io: IO = {
-    sendOutput: sendOutput,
-    getInput: sinon.spy(),
-  };
+function note(note: Note["note"], octave: Note["octave"]): Note {
+  return { note, octave };
+}
+
+function setupTest(program: Instruction[]) {
+  const getInput = jest.fn();
+  const sendOutput = jest.fn();
+  const io: IO = { sendOutput, getInput };
 
   const interpreter = new Interpreter(
     new SparseTape(),
-    { play: sinon.spy(), reset: sinon.spy() },
-    { next: sinon.stub().returns(Promise.resolve()), reset: sinon.spy() },
-    // c4 b3 c4 c4 c4 ( d4 c4 c4 )
-    new ArraySource([
-      { note: "C", octave: 4 },
-      { note: "D", octave: 4 },
-      { note: "D", octave: 4 },
-      { note: "D", octave: 4 },
-      { note: "D", octave: 4 },
-      Rest,
-    ]),
+    { play: jest.fn(), reset: jest.fn() },
+    { next: jest.fn().mockReturnValue(Promise.resolve()), reset: jest.fn() },
+    new ArraySource(program),
     io,
   );
 
+  return {
+    interpreter,
+    sendOutput,
+    getInput,
+  };
+}
+
+it('can increment and send output', async () => {
+  const { interpreter, sendOutput } = setupTest([
+      note("C", 4),
+      note("D", 4),
+      note("D", 4),
+      note("D", 4),
+      note("D", 4),
+      Rest,
+    ]);
+
   await interpreter.perform();
 
-  expect(sendOutput.calledWith(3)).toEqual(true);
+  expect(sendOutput).toHaveBeenCalledTimes(1);
+  expect(sendOutput).toHaveBeenCalledWith(3);
+});
+
+it('can take input', async () => {
+  const { interpreter, sendOutput, getInput } = setupTest([
+      note("C", 4),
+      note("B", 4),
+      Rest,
+      note("A", 4),
+      note("B", 4),
+      Rest,
+    ]);
+
+  getInput.mockReturnValueOnce(Promise.resolve(5));
+
+  await interpreter.perform();
+
+  expect(getInput).toHaveBeenCalledTimes(1);
+  expect(sendOutput).toHaveBeenCalledTimes(1);
+  expect(sendOutput).toHaveBeenCalledWith(5);
+});
+
+it('can loop', async () => {
+  const { interpreter, sendOutput } = setupTest([
+      note("C", 4),
+      note("D", 4),
+      note("D", 4),
+      note("D", 4),
+      note("D", 4),
+      LoopStart,
+      note("E", 4),
+      note("D", 4),
+      note("D", 4),
+      note("C", 4),
+      note("D", 4),
+      Rest,
+      LoopEnd,
+    ]);
+
+  await interpreter.perform();
+
+  expect(sendOutput).toHaveBeenCalledTimes(3);
+  expect(sendOutput).nthCalledWith(1, 2);
+  expect(sendOutput).nthCalledWith(2, 1);
+  expect(sendOutput).nthCalledWith(3, 0);
+});
+
+it('skips loop when value is 0', async () => {
+  const { interpreter, sendOutput } = setupTest([
+      note("C", 4),
+      note("D", 4),
+      LoopStart,
+      note("E", 4),
+      note("D", 4),
+      note("D", 4),
+      note("C", 4),
+      note("D", 4),
+      Rest,
+      LoopEnd,
+    ]);
+
+  await interpreter.perform();
+
+  expect(sendOutput).toHaveBeenCalledTimes(0);
+});
+
+it('skips nested loops when value is 0', async () => {
+  const { interpreter, sendOutput } = setupTest([
+      note("C", 4),
+      note("D", 4),
+      LoopStart,
+      note("E", 4),
+      note("D", 4),
+      LoopStart,
+      note("D", 4),
+      LoopStart,
+      LoopEnd,
+      note("C", 4),
+      LoopEnd,
+      note("D", 4),
+      Rest,
+      LoopEnd,
+    ]);
+
+  await interpreter.perform();
+
+  expect(sendOutput).toHaveBeenCalledTimes(0);
 });
